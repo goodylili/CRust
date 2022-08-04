89fee9eba9125e7f03ab50f1bc34eedb2988857e
Goodnessuc <ukejegoodness599@gmail.com>
2022-08-04 18:36:51 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,3 @@
-// use rocket::response::content::Json;
-//
-// #[derive(Debug, Deserialize, Serialize)]
-// #[serde(crate = "rocket::serde")]
-// pub struct Bio {
-//     pub name: String,
-//     pub age: i32,
-// }
-//
-// // create get-current-date route under /date and call get_current_date service which will return a Date object
-// // route returns a Date object converted to JSON
-// #[get("/date/get-current-date")]
-// pub fn get_current_date() -> Json<&'static str> {
-//     Json("{}")
-// }
-//
-//
-//
-//
-//
-//
-
-// stopped tutorial at 18:28
 
 fn main(){
 
