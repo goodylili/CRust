2eef9479a1b5998a3c2a4f33ab64dd4500d965f4
Goodnessuc <ukejegoodness599@gmail.com>
2022-08-04 21:13:38 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+#![feature(decl_macro)]
+use rocket::*;
+use rocket::response::content::Json;
 
-fn main(){
 
+#[derive(FromForm, Debug)]
+struct Book {
+    first_name: String,
+    last_name: String,
+    phone: i32,
 }
+
+
+
+
+
+fn main() {
+    rocket::ignite()
+        .mount("/api", routes![hello])
+        .launch();
+}
+
+
+// git add .
+// git commit -m changes
+// git push origin main
