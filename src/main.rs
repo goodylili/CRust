d9a3a77feb346d90911a9b10252ceabe237edc22
Goodnessuc <ukejegoodness599@gmail.com>
2022-08-09 10:46:28 +0100

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 #![feature(decl_macro)]
 use rocket::*;
 use serde::Deserialize;
+use serde::Serialize;
+
 use rocket_contrib::json::Json;
 
+
+
 //Configs to make struct serializable and debug able
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Serialize)]
 pub struct BioData {
     pub name: String,
     pub age: i32,
 }
+
+
+
+#[post("/todo", format = "json", data = "<bio>")]
+fn create_bio(bio: Json<BioData>) -> Json<BioData> {
+    let person = BioData{
+        name: bio.name,
+        age: bio.age
+    };
+    bio
+
+}
+
 
 #[get("/hello")]
 fn hello() -> Json<&'static str> {
@@ -18,18 +35,9 @@ fn hello() -> Json<&'static str> {
 
 
 
-#[post("/todo", format = "json", data = "<bio>")]
-fn create_bio(bio: Json<BioData>) -> String {
-    format!("{:?}",bio)
-
-}
-
-
 fn main() {
     rocket::ignite()
         .mount("/api", routes![create_bio, hello])
         .launch();
 }
-
-//
 
