5f134a23a02448b2bc6bdd6ce109605eceeef774
Goodnessuc <ukejegoodness599@gmail.com>
2022-08-09 11:13:30 +0100

    added vector store

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![feature(decl_macro)]
+
 use rocket::*;
+use rocket_contrib::json::Json;
 use serde::Deserialize;
 use serde::Serialize;
-
-use rocket_contrib::json::Json;
-
-
 
 //Configs to make struct serializable and debug able
 #[derive(Deserialize, Serialize)]
@@ -14,17 +12,21 @@ struct BioData {
     username: String,
 }
 
+fn vec_db() -> Vec<BioData> {
+    let vec_store: Vec<BioData> = Vec::new();
+    return vec_store;
+}
 
 
 #[post("/todo", format = "json", data = "<bio>")]
 fn create_bio(bio: Json<BioData>) -> Json<BioData> {
-    let person = BioData{
+    let person = BioData {
         age: bio.age,
         username: bio.username.to_string(),
     };
+    vec_db().push(person);
 
     bio
-
 }
 
 
@@ -33,7 +35,6 @@ fn hello() -> Json<&'static str> {
     Json("{'status' : 'successful',
 'message`: 'Your first Rocket app is live'}")
 }
-
 
 
 fn main() {
